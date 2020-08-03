d73b285fd16eca41afd5b272e39218f3c68d8d9a
kimch0612 <kimch061279@gmail.com>
2020-08-04 00:33:39 +0900

    API Requests & Response Test

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello World!")
+import urllib.request
+import json
+
+with open('account.json', 'rt', encoding='UTF8') as json_file:
+    json_data = json.load(json_file)
+    id = json_data["id"]
+    pw = json_data["pw"]
+
+urllib.request.urlretrieve("https://url/webapi/auth.cgi?api=SYNO.API.Auth&version=2&method=login&account=%s&passwd=%s&session=DownloadStation&format=cookie" % (id, pw), "login.json")
+urllib.request.urlretrieve("https://url/webapi/DownloadStation/task.cgi?api=SYNO.DownloadStation.Task&version=1&method=list&format=cookie", "list.json")
+
+with open('list.json', 'rt', encoding='UTF8') as json_file:
+    json_data = json.load(json_file)
+    dl_list = json_data["title"]
+
+print(dl_list)
