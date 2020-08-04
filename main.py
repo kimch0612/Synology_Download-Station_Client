9746cfa652ee1928d9fa5dff6e1a885782772608
kimch0612 <kimch061279@gmail.com>
2020-08-04 11:52:07 +0900

    Add marget download support (line 51 to 53 is not working)

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ with open('account.json', 'rt', encoding='UTF8') as json_file:
     id = json_data["id"]
     pw = json_data["pw"]
 
+link = input("Please typing url what you want to download : ")
+file = input("Please drop down Torrent File (or typing it's physical directory directions) : ")
+
 login_session = "https://your-nas-domain/webapi/auth.cgi?api=SYNO.API.Auth&version=2&method=login&account=%s&passwd=%s&session=DownloadStation&format=cookie" % (id, pw)
 download_task = "https://your-nas-domain/webapi/DownloadStation/task.cgi?api=SYNO.DownloadStation.Task&version=1&method=list&format=cookie"
 
@@ -29,8 +32,30 @@ data = json.loads(title)
 with open('list.json', 'w', encoding='utf-8') as make_file:
     json.dump(title, make_file, ensure_ascii=False)
 
-# Code below needs to be fixed
-#with open('list.json', 'rt', encoding='UTF-8') as json_file:
-#    content = json.load(json_file)
-#api_list = [x for x in content['data']['tasks']['title'].keys()]
-#print(api_list)
+print(data)
+
+print("Now downloading file(s) now..")
+download_link = "https://your-nas-domain/webapi/DownloadStation/task.cgi?api=SYNO.DownloadStation.Task&version=1&method=create&uri=%s" % (link)
+
+driver.get(download_link)
+delay = 2
+
+driver.get(download_task)
+delay = 2
+title = driver.find_element_by_tag_name("pre").text
+data = json.loads(title)
+with open('list.json', 'w', encoding='utf-8') as make_file:
+    json.dump(title, make_file, ensure_ascii=False)
+print(data)
+
+download_file = "https://your-nas-domain/webapi/DownloadStation/task.cgi?api=SYNO.DownloadStation.Task&version=1&method=create&file=%s" % (file)
+driver.get(download_file)
+delay = 2
+
+driver.get(download_task)
+delay = 2
+title = driver.find_element_by_tag_name("pre").text
+data = json.loads(title)
+with open('list.json', 'w', encoding='utf-8') as make_file:
+    json.dump(title, make_file, ensure_ascii=False)
+print(data)
